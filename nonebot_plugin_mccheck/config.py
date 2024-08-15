93131423d3a30aecfee57f84c5f1b4f46b506c65
molanp <molanp>
2024-08-15 22:27:30 +0800

    修复配置前缀，正确导入字体

--- nonebot_plugin_mccheck/config.py
+++ nonebot_plugin_mccheck/config.py
@@ -4,14 +4,14 @@ from nonebot.plugin import get_plugin_config
 
 class ScopedConfig(BaseModel):
 
-    mcc_language: str = Field(default="zh-cn")
+    language: str = Field(default="zh-cn")
     """插件渲染图片所使用的语言"""
-    mcc_type: int = Field(default=0)
+    type: int = Field(default=0)
     """插件发送的消息类型"""
 
 
 class Config(BaseModel):
-    status: ScopedConfig = Field(default_factory=ScopedConfig)
+    mcc: ScopedConfig = Field(default_factory=ScopedConfig)
     """MCCheck Config"""
 
 
