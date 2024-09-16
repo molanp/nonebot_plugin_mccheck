92bbbaec7737f8d5b8bf839124e60ef018df2d89
molanp <104612722+molanp@users.noreply.github.com>
2024-09-17 02:21:47 +0800

    重构和修复nonebot_plugin_mccheck的代码
    对`nonebot_plugin_mccheck`的代码进行了重构和错误修复。更新了初始化文件，移除了未使用的导入，优化了异常处理和语言配置管理。对`untils.py`进行了调整，改进了图像处理、DNS解析和MC服务器信息获取的逻辑。整体上提高了代码的稳定性和性能。
    
    解决了DNS解析失败和连接超时的问题，改进了异常报告机制。对服务器查询结果的构建和发送机制进行了重构，提高了消息格式的灵活性和可读性。更新了语言文件，修正了部分翻译，并增强了多语言支持。
    
    此次更新还涉及到了插件的配置和依赖管理，确保了与NoneBot框架的兼容性。整体代码结构更加清晰，便于未来的维护和功能扩展。
    
    BREAKING CHANGE: 由于对服务器信息获取和消息构建机制进行了重构，插件的内部API可能有变动，这可能会影响依赖此插件的其他组件或脚本。

---
+++ nonebot_plugin_mccheck/configs.py
@@ -0,0 +1,11 @@
+from .config import config as plugin_config
+import os
+import ujson
+
+def readInfo(file: str) -> dict:
+    with open(os.path.join(os.path.dirname(__file__), file), "r", encoding="utf-8") as f:
+        return ujson.loads((f.read()).strip())
+
+message_type = plugin_config.type
+lang = plugin_config.language
+lang_data = readInfo("language.json")
