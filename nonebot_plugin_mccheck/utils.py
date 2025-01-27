862b79fc1ffb4fd1063bebe97e94876a3e55aa79
molanp <104612722+molanp@users.noreply.github.com>
2025-01-27 23:52:56 +0800

    refactor(mccheck): 优化代码逻辑和结构
    - 移除了不必要的导入
    - 修改了 ip_type 的检查方式，使其更灵活
    - 优化了 SRV 记录解析的逻辑
    - 删除了多余的打印语句
    - 更新了插件版本号至 0.1.43

--- nonebot_plugin_mccheck/utils.py
+++ nonebot_plugin_mccheck/utils.py
@@ -13,7 +13,6 @@ from nonebot import require, logger
 from PIL import Image, ImageDraw, ImageFont
 import ujson
 
-from zhenxun.services.log import logger
 
 from .configs import VERSION, lang, lang_data, message_type
 from .data_source import ConnStatus, MineStat, SlpProtocols
@@ -167,7 +166,7 @@ async def get_mc(
     - list: 包含Java版和Bedrock版服务器信息的列表。
     """
     loop = asyncio.get_event_loop()
-    if ip_type == "SRV":
+    if ip_type.startswith("SRV"):
         return [await loop.run_in_executor(None, get_java, ip, port, ip_type, timeout)]
     return [
         await loop.run_in_executor(None, get_java, ip, port, ip_type, timeout),
@@ -229,8 +228,7 @@ def get_bedrock(
     返回:
     - MineStat实例，包含服务器状态信息，如果服务器在线的话；否则可能返回None。
     """
-    v6 = ip_type == "IPv6"
-    print(f"Getting Bedrock server status: {host}:{port} v6 {v6}")
+    v6 = "IPv6" in ip_type
     result = MineStat(host, port, timeout, SlpProtocols.BEDROCK_RAKNET, v6)
 
     if result.online:
@@ -252,8 +250,7 @@ def get_java(
     返回:
     - MineStat 实例，包含服务器状态信息，如果服务器在线的话；否则可能返回 None。
     """
-    v6 = ip_type == "IPv6"
-    print(f"Getting Java server status: {host}:{port} v6 {v6}")
+    v6 = "IPv6" in ip_type
 
     # Minecraft 1.4 & 1.5 (legacy SLP)
     result = MineStat(host, port, timeout, SlpProtocols.LEGACY, v6)
@@ -427,10 +424,9 @@ async def get_origin_address(
                 srv_address = str(rdata.target).rstrip(".")
                 srv_port = rdata.port
                 ip_type = await get_ip_type(srv_address)
-                print(f"SRV record found: {srv_address}:{srv_port} ({ip_type})")
                 if ip_type == "Domain":
                     srv_address = await get_origin_address(srv_address, srv_port, False)
-                    data.extend(srv_address)
+                    data.extend([(addr, port, f"SRV-{ip_type}") for addr, port, ip_type in srv_address])
                 else:
                     data.append((srv_address, srv_port, "SRV"))
 
@@ -440,7 +436,6 @@ async def get_origin_address(
         ):
             response = await resolver.resolve(domain, "AAAA")
             for rdata in response:
-                print(f"AAAA record found: {rdata.address!s}")
                 data.append((str(rdata.address), ip_port, "IPv6"))
 
     async def resolve_a():
@@ -449,7 +444,6 @@ async def get_origin_address(
         ):
             response = await resolver.resolve(domain, "A")
             for rdata in response:
-                print(f"A record found: {rdata.address!s}")
                 data.append((str(rdata.address), ip_port, "IPv4"))
 
     if is_resolve_srv:
