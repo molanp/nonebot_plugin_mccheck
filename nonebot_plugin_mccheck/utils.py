b38b02dddf60d57e138e2bc5f92e921519771fef
molanp <molanp>
2025-01-30 01:11:10 +0800

    修复同服ip负载均衡造成的重复查询

--- nonebot_plugin_mccheck/utils.py
+++ nonebot_plugin_mccheck/utils.py
@@ -402,9 +402,11 @@ async def get_origin_address(
                 ip_type = await get_ip_type(srv_address)
                 if ip_type == "Domain":
                     srv_address_ = await get_origin_address(srv_address, srv_port, False)
-                    data.extend([(addr, port, f"SRV-{ip_type}", refer) for addr, port, ip_type, refer in srv_address_])
+                    data.extend([(srv_address_[0][0], srv_address_[0][1], f"SRV-{srv_address_[0][2]}", srv_address_[0][3])])
+                    #data.extend([(addr, port, f"SRV-{ip_type}", refer) for addr, port, ip_type, refer in srv_address_])
                 else:
                     data.append((srv_address, srv_port, "SRV", domain))
+                break
 
     async def resolve_aaaa():
         with contextlib.suppress(
@@ -413,6 +415,7 @@ async def get_origin_address(
             response = await resolver.resolve(domain, "AAAA")
             for rdata in response:
                 data.append((str(rdata.address), ip_port, "IPv6", domain))
+                break
 
     async def resolve_a():
         with contextlib.suppress(
@@ -421,6 +424,7 @@ async def get_origin_address(
             response = await resolver.resolve(domain, "A")
             for rdata in response:
                 data.append((str(rdata.address), ip_port, "IPv4", domain))
+                break
 
     if is_resolve_srv:
         await asyncio.gather(resolve_srv(), resolve_aaaa(), resolve_a())
