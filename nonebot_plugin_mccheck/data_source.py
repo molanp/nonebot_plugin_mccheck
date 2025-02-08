68868961899bbc999d21266e5c52e7e8629af31d
molanp <molanp>
2025-02-08 15:09:43 +0800

    使用版本号欺骗以从MiniMOTD正确获取渐变数据

--- nonebot_plugin_mccheck/data_source.py
+++ nonebot_plugin_mccheck/data_source.py
@@ -701,8 +701,8 @@ class MineStat:
 
         # Construct Handshake packet
         req_data = bytearray([0x00])
-        # Add protocol version. If pinging to determine version, use `-1`
-        req_data += bytearray([0xFF, 0xFF, 0xFF, 0xFF, 0x0F])
+        # Add protocol version. If pinging to determine version, use `25565`
+        req_data += bytearray([0xdd, 0xc7, 0x01])
         # Add server address length
         req_data += self._pack_varint(len(self.refer))
         # Server address. Encoded with UTF8
